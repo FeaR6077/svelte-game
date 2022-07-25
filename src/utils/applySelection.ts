de2441f77dd4b98687e313dedfde5038a2f09995
FeaR6077 <julian-pretzsch@web.de>
2022-07-25 21:05:24 +0200

    solve 20/50 lint issues and make linter jappy

--- src/utils/applySelection.ts
+++ src/utils/applySelection.ts
@@ -1,6 +1,9 @@
-const filterByCategory = (subset, category) => {
-  const filteredCelebs = [];
+import type { Celebrity } from "../Components/types/game_data_type"
+import type { GameCategory } from "../Components/types/game_category"
+import type { GamePairs } from "../Components/types/game_pairs"
 
+const filterByCategory = (subset: Array<Celebrity>, category: GameCategory) => {
+  const filteredCelebs: Array<Celebrity> = [];
   subset.forEach((element) => {
     if (element.categories.includes(category.slug)) {
       filteredCelebs.push(element);
@@ -10,7 +13,7 @@ const filterByCategory = (subset, category) => {
   return filteredCelebs;
 };
 
-const getRandomNumber = (max) => {
+const getRandomNumber = (max: number) => {
   return Math.floor(Math.random() * (max + 1));
 };
 
@@ -19,8 +22,9 @@ const getRandomNumber = (max) => {
 // fetch similar celebs if possible as gamepair
 
 
-const createRandomGamePair = (filteredCelebs) => {
-  const filteredCelebPairs = [];
+const createRandomGamePair = (filteredCelebs: Array<Celebrity>) => {
+
+  const filteredCelebPairs: Array<GamePairs>= [];
 
   for (let x = 0; x < 10; x++) {
     filteredCelebPairs.push({
