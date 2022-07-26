acf31eabe23b04818ad4b52443bfe7c291302f94
FeaR6077 <julian-pretzsch@web.de>
2022-07-26 09:10:46 +0200

    create raw result card

---
+++ src/Components/Results.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { beforeUpdate } from 'svelte';
+
+    import ResultCard from "./ResultCard.svelte";
+
+    export let celebsGamePairs
+    export let stage
+    export let correctGuess
+    
+    let results = [];
+
+    $: if(correctGuess, stage){
+            results[stage -1] = {stage: correctGuess}, console.log(results)}
+
+    $: console.log(stage, correctGuess)
+
+
+    onMount(() => {
+        for(let i = 1; i <= celebsGamePairs; i++){
+            results.push(
+                {i: "undefined"}
+            )
+        }
+    })
+
+</script>
